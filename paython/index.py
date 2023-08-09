8e6a107144e2093e4454c8e6d3e94cf69d061c96
mayur cahvda <140694981+mayurchavda3302@users.noreply.github.com>
2023-08-09 05:52:46 -1200

    Update index.py

--- paython/index.py
+++ paython/index.py
@@ -18,23 +18,12 @@ all_links=[]
 for link in anchores:
     linktext=url+link.get('href')
     all_links.append(link)
-    # print(linktext)
 
-'''
-when we save all url to the csv file than links does not stroge in the set()
-it wii show error becuase set has only unique elements and it wii not preserve the insertion.
-
-so we store the data in list
-'''
-
-
-data={'link':all_links}
-# here the link is key and  all_links has value
-df=pd.DataFrame(data)
-# here we create the data frame 
 csv_file="links.csv"
+with open(csv_file,'w',newline='',encoding='utf-8') as file:
+    csv_write=csv.writer(file)
+    csv_write.writerow(['links'])
+    for links in all_links :
+        csv_write.writerow([link])
 
-df.to_csv(csv_file,index=False,encoding='utf-8')
-# here the data wii in character/string form so wii wii send it in 'utf-8'.
-#  
-print(f"the links wii storge in the :{csv_file}  file")       
+print(f"the links  wii be store in {csv_file} file ")
