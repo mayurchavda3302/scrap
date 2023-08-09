22bbd521fa266abd0cdc32a2bb678d598a75fd03
mayur cahvda <140694981+mayurchavda3302@users.noreply.github.com>
2023-08-09 06:41:39 -1200

    Update index.py

--- index.py
+++ index.py
@@ -1,28 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+
 import time
-
+import csv
 url="https://ecolabelindex.com/ecolabels/"
-
 r=requests.get(url)
 time.sleep(2)
 htmlcontent=r.content
 soup=BeautifulSoup(htmlcontent,'html.parser')
-
-
 anchores=soup.find_all('a')
 all_links=[]
 
-for link in anchores:
-    linktext=url+link.get('href')
-    all_links.append(link)
+
+for links in anchores:
+    linktext=url+links.get('href')
+    all_links.append(linktext)
+    
 
 csv_file="links.csv"
 with open(csv_file,'w',newline='',encoding='utf-8') as file:
     csv_write=csv.writer(file)
     csv_write.writerow(['links'])
-    for links in all_links :
+    for link in all_links:
         csv_write.writerow([link])
 
 print(f"the links  wii be store in {csv_file} file ")
+
