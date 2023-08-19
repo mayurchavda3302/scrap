fa94dd57d3859d35464e39214d8b4d5d25f98011
mayur cahvda <140694981+mayurchavda3302@users.noreply.github.com>
2023-08-19 10:28:10 -1200

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,29 @@
 import requests
-import time
 from bs4 import BeautifulSoup
+import time 
 import csv
+url="https://ecolabelindex.com/ecolabels/"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+r=requests.get(url,headers=headers)
+time.sleep(3)
+soup=BeautifulSoup(r.text,'html.parser')  
+span=soup.find("div",{"class":"ecolabel-result article"})
 
-r=requests.get('https://www.geeksforgeeks.org/')
-time.sleep(2)
-all_link=[]
-soup=BeautifulSoup(r.content,'html.parser')
-for link in soup.find_all('a'):
-    link= link.get('href')
-    all_link.append(link)
-    # print(link)
-    csv_flie='links.csv'    
-    with open(csv_flie,'w')as file:
-      csv_write = csv.writer(file)
-      csv_write.writerow(['links'])
-      for linktext in all_link:
-            csv_write.writerow([linktext])
-      
+unique_links=set()
+for links in  span.find_all('a'):
+    
+    link=links.get('href')
+    if link:
+        unique_links.add(link)
+   
+csv_file="links.csv"
+with open(csv_file,"w",newline="") as file:
+    csv_write=csv.writer(file)
+
+    for linktext in unique_links:
+         if linktext.startswith("/"):
+             complate_link=f"https://ecolabelindex.com{linktext}"
+             csv_write.writerow([complate_link])
+         else:
+             csv_write.writerow([linktext])
+
